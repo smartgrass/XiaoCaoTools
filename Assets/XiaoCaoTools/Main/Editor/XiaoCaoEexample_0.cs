85893b8d71a150f75892fe2daf553194334d6411
DESKTOP-6P4QJO8\Nasa <nasa@battlecryhq.com>
2022-12-28 18:03:43 +0800

    其他窗口样式

---
+++ Assets/XiaoCaoTools/Main/Editor/XiaoCaoEexample_0.cs
@@ -0,0 +1,27 @@
+﻿using NaughtyAttributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using XiaoCao;
+using Object = UnityEngine.Object;
+
+public class XiaoCaoEexample_0 : XiaoCaoWindow
+{
+    [MenuItem("Tools/XiaoCao/Eexample_0")]
+    static void Open()
+    {
+        OpenWindow<XiaoCaoEexample_0>("窗口名字");
+    }
+
+    [Button("执行事件")]
+    private void DoEvent()
+    {
+        Debug.Log($"yns do Event");
+    }
+
+}
