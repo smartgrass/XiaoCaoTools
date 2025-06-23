6a90d5bf9870d68fa1d717cf544b6f490fb08fa4
smartgrass <30657539+smartgrass@users.noreply.github.com>
2025-06-24 05:14:46 +0800

    update untiy-> 2021.3.34f1c1

--- BuildTools/Build.bat
+++ BuildTools/Build.bat
@@ -3,15 +3,15 @@ set filename=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
 
 set "filename=%filename: =0%"
 
-set targetProject=E:\MyApplication2\EmptyProject\XiaoCaoTools
+set targetProject=D:\Github\XiaoCaoTools
+
+set UnityExePath="C:\Program Files\Unity\Hub\Editor\2021.3.34f1c1\Editor\Unity.exe"
 
 echo Start Build %targetProject%
 
-cd D:\unity\2020.3.8f1c1\Editor
-
 REM pause
 
-.\Unity.exe -projectPath %targetProject% -quit -batchmode -executeMethod CIBuild.Build -logFile %filename%_buildApk.log
+%UnityExePath% -projectPath %targetProject% -quit -batchmode -executeMethod CIBuild.Build -logFile %filename%_buildApk.log
 
 
 REM %1 -projectPath %2 -quit -batchmode -executeMethod APKBuild.Build -logFile build.log
@@ -22,11 +22,13 @@ if not %errorlevel%==0 ( goto fail ) else ( goto success )
 echo Build APK OK
 REM Copr Dir
 goto end
- 
+
 :fail
 echo Build APK Fail
+
 goto end
+
  
 :end
-
+pause
 REM pause
